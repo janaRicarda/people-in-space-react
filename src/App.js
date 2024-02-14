53313562ed12fb458189301499f56d234a179b7a
Jana May <jana.ricarda.may@gmail.com>
2024-02-14 20:12:58 +0100

    solved

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [numberInSpace, setNumberInSpace] = useState(0);
+  const [peopleInSpace, setPeopleInSpace] = useState([]);
+  const [activeCraft, setActiveCraft] = useState("All");
+
+  useEffect(() => {
+    async function fetchPeopleInSpace() {
+      const response = await fetch("http://api.open-notify.org/astros.json");
+      const data = await response.json();
+      setNumberInSpace(data.number);
+      setPeopleInSpace(data.people);
+    }
+    fetchPeopleInSpace();
+  }, []);
+
+  const filteredPeople =
+    activeCraft === "All"
+      ? peopleInSpace
+      : peopleInSpace.filter((person) => person.craft === activeCraft);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>People in Space: {numberInSpace}</h1>
+      <h2>active craft : {activeCraft}</h2>
+      <section>
+        <button type="button" onClick={() => setActiveCraft("All")}>
+          All
+        </button>
+        <button type="button" onClick={() => setActiveCraft("ISS")}>
+          ISS
+        </button>
+        <button type="button" onClick={() => setActiveCraft("Tiangong")}>
+          Tiangong
+        </button>
+      </section>
+      {filteredPeople.length > 0 && (
+        <ul>
+          {filteredPeople.map((person) => (
+            <li key={person.name}>{person.name}</li>
+          ))}
+        </ul>
+      )}
+    </main>
   );
 }
 
